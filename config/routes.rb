c4349fe45ea0d75af52ea3d75d4d00d03900562e
Alexey Lebedev <alexeylebedev1995@gmail.com>
2022-11-22 16:48:35 +0200

    edited the booking confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :fans do
-    resources :bookings, omly: [:show, :new, :create, :update]
+    resources :bookings, only: [:show, :new, :create, :update]
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
