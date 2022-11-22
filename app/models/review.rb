1ecda540cce76d140e700fed7cd541378400b565
Eoin Farren <eoinfarren@gmail.com>
2022-11-22 14:57:16 +0100

    Review Validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { in: (0..5) }
 end
