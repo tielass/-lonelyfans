bd35d485a025fedf7538a9274ab032eed2233c0d
Alexey Lebedev <alexeylebedev1995@gmail.com>
2022-11-23 18:04:31 +0200

    commented validation

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,11 +1,11 @@
 class Fan < ApplicationRecord
   NATIONS = ['qatar', 'ecuador', 'senegal', 'netherlands', 'spain', 'costa rica', 'germany', 'japan', 'england', 'iran', 'us', 'wales', 'belgium', 'canada', 'morocco', 'coratia', 'argentinia', 'saudi arabia', 'mexico', 'poland', 'brazil', 'serbia', 'switzerland', 'cameroon', 'france', 'australia', 'denmark', 'tunisia', 'portugal', 'ghana', 'uruguay', 'south korea']
   CATEGORY = ['supportive', 'violent', 'loud', 'serious', 'settled', 'good behaviour', 'drunk']
-  validates :nationality, inclusion: { in: NATIONS }
-  validates :category, inclusion: { in: CATEGORY }
-  validates :image_url, :name, :price, presence: true
-  validates :description, length: { in: 8..300 }
-  validates :name, uniqueness: true
+  # validates :nationality, inclusion: { in: NATIONS }
+  # validates :category, inclusion: { in: CATEGORY }
+  # validates :image_url, :name, :price, presence: true
+  # validates :description, length: { in: 8..300 }
+  # validates :name, uniqueness: true
   belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
