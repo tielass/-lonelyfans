6ae565d37f0c5881f6eacdb01b3c392b1557b5b3
Mihail Mihaylov <tielass@gmail.com>
2022-11-23 16:37:51 +0100

    Added styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,17 +14,19 @@ puts "inserting fans"
 
 12.times do
   new_user = User.create!(email: Faker::Internet.email, password: "password")
-  new_fan = Fan.create!(name: Faker::Marketing.buzzwords, nationality: Faker::Nation.nationality, description: Faker::ChuckNorris.fact, category: Faker::Fantasy::Tolkien.poem, price:  rand(10..100), user: new_user )
+  # new_fan = Fan.create!(name: Faker::Marketing.buzzwords, nationality: Faker::Nation.nationality, description: Faker::ChuckNorris.fact, category: Faker::Fantasy::Tolkien.poem, price:  rand(10..100), user: new_user )
   #user.photo.attached?
 
-  new_fan = Fan.create!(name: Faker::Marketing.buzzwords, nationality: Faker::Nation.nationality, description: Faker::ChuckNorris.fact, category: ['supportive', 'violent', 'loud', 'serious', 'settled', 'good behaviour', 'drunk'].sample(1).join(''), price:  rand(10..100), user: new_user )
+  new_fan = Fan.create!(name: Faker::Marketing.buzzwords, nationality: Fan::NATIONS.sample, description: Faker::ChuckNorris.fact, category: Fan::CATEGORY.sample, price:  rand(10..100), user: new_user)
+
+  # file = URI.open('https://source.unsplash.com/random/900%C3%97700/?fans')
+  # fan.photo.attach(io: file, filename: 'user.png', content_type: 'image/png')
 
   # file = URI.open("https://source.unsplash.com/random/900%C3%97700/?fans")
   # article = Article.new(title: "fans title", body: "A great console")
   # article.photo.attach(io: file, filename: "fan.png", content_type: "image/png")
   # article.save
-  # user.photo.attached?
-
+  # fan.photo.attached?
   puts "#{new_fan.name} was sucessfully created!"
 end
 
