987e161cadb2a3a8f13ba251fdc0a48736cf9ce4
Paul Sessbruegger <paul.at.home@gmx.de>
2022-11-24 14:18:36 +0100

    changed column image_url to photo and save between cloudinary setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_102224) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_111349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_102224) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
+    t.string "photo"
     t.float "latitude"
     t.float "longitude"
     t.string "address"
